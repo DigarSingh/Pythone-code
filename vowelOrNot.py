f9dd2fc5d5bc40af90b11df960079d76f2537ec7
BYTE90 <digarsingh2004@gmail.com>
2024-12-07 22:49:27 +0530

    Vowel Or Not

---
+++ vowelOrNot.py
@@ -0,0 +1,6 @@
+char=input("Enter the character: ")
+
+if char in ('a','e','i','o','u','A','E','I','O','U'):
+    print("The character is a vowel")
+else:
+     print("The character is not a vowel")
